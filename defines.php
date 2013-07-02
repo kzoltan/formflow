c905eabb2be68718231ae9d0cc07c708d6e807c9
Zoltan Kisgyorgy <zoltan.kisgyorgy@gmail.com>
2013-07-02 10:48:17 +0300

    rezolve conflict from windows app

--- defines.php
+++ defines.php
@@ -1,6 +1,10 @@
 <?php 
 
+<<<<<<< HEAD
 /* PHP DON'T SUCKS */
+=======
+/* PHP SUCKS, SUCKS */
+>>>>>>> fda9825fb2a582c8eeae56e8f7693fe6e51b2d11
 
 // Database access data
 define('__DB_USER', 'root');
