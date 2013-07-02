ec52a1c2bdfecee3dcc6b3dbac1dd913608c1506
aniela.raileanu <aniela.raileanu@reea.net>
2013-07-02 12:18:55 +0300

    index-include

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
-<?php 
-
+<?php  
+error_reporting (E_ALL);
 include 'helpers.php';
 include 'form.php';
 
 //do_mysqli_connect();
 do_mysqli_connect() or die ('I can not to connect to MySQL!');
 
-if(isset($_POST) && !empty($_POST['isPost'])) {
-    
-    $validate = validate_form($_POST);
-    if ($validate === TRUE) {
-	$save = saveFieldsInDB($_POST);
-	if($save === TRUE) {
-            print "User saved!";
-            unset($_POST);
-            do_mysqli_close();
-	}
-    }
-    else{
-        print $validate;
-    } 
-}
 $fields = get_fields();
 $form = get_form($fields);
 
 print $form;
 
-$build = render_form($form, $form_state);
+if (isset($_POST) && !empty($_POST['isPost'])) {
+    $validate = validate_form($_POST);
+    if ($validate === TRUE) {
+        submit_form($_POST);
+    }
+    else {
+        print $validate;
+    }
+}
+
+$build = render_form($form, $validate);
 print $build;
 
 ?>
