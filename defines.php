e43e0434dedc4a4d600f22912650a80ea2255c0e
Zoltan Kisgyorgy <zoltan.kisgyorgy@gmail.com>
2013-07-02 10:51:04 +0300

    lets hope this works

--- defines.php
+++ defines.php
@@ -1,14 +1,6 @@
 <?php 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* PHP DON'T SUCKS */
-=======
-/* PHP SUCKS, SUCKS */
->>>>>>> fda9825fb2a582c8eeae56e8f7693fe6e51b2d11
-=======
-/* PHP SUCKS, SUCKS */
->>>>>>> fda9825fb2a582c8eeae56e8f7693fe6e51b2d11
+/* DOING GIT ON WINDOWS USING THERE APP IS EVEN WORST */
 
 // Database access data
 define('__DB_USER', 'root');
