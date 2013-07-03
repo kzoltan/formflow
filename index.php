4c096296dc023e9255b32e136603353dc1988f67
aniela.raileanu <aniela.raileanu@reea.net>
2013-07-03 12:35:04 +0300

    index modify

--- index.php
+++ index.php
@@ -8,26 +8,25 @@ include 'form.php';
 //include database class
 include 'database.class.php';
 
-
 //instantiate new database
+
 $database = new Database();
 
-$database->$fields = 'hello';
-print $database->$fields;
-
 //objects
-$database->__construct();
-
-$query = $database->db_select('tablename')->fields('field names')->conditions('conditions')->execute();
+/*
+$query = $database->db_select('tablename')->fields('fieldname')->condition('conditioname')->execute();
 echo "<pre>";
 print $query;
 echo "</pre>";
+*/
 
+
+/*
 $result = $database->resultset();
 echo "<pre>";
 print $result;
 echo "</pre>";
-
+*/
 
 $fields = get_fields();
 $form = get_form($fields);
